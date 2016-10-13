cda4e75876bb5e350786f880124ce71061cc869d
Tom LaTulipe <tlatulipe@gmail.com>
2016-10-13 13:04:00 -0400

    Finished setting up the difficulty and volume sliders. Preferences should now correctly save on exit. Default values button works. Thumbs up good job.

--- Assets/Scripts/OptionsController.cs
+++ Assets/Scripts/OptionsController.cs
@@ -9,7 +9,8 @@ public class OptionsController : MonoBehaviour {
     public Slider difficultySlider;
     public LevelManager levelManager;
 
-    private MusicPlayer musicManager; // if this is correct, it's because I named my musicmanager MuiscPlayer
+    private MusicPlayer musicManager; 
+    // if this is correct, it's because I named my musicmanager MuiscPlayer
 
     float volumeLevel;
     float currentVolume;
@@ -19,17 +20,12 @@ public class OptionsController : MonoBehaviour {
     GameObject easyDiff;
     GameObject mediumDiff;
     GameObject hardDiff;
-
-    //Text easyDiff;
-    //Text mediumDiff;
-    //Text hardDiff;
-
+  
 	// Use this for initialization
 	void Start ()
     {
         // Testing the difficulty settings
-
-        PlayerPrefsManager.SetDifficulty(0.5f);
+        //PlayerPrefsManager.SetDifficulty(0.1f);
 
         easyDiff = GameObject.Find("Easy");
         mediumDiff = GameObject.Find("Medium");
@@ -38,28 +34,21 @@ public class OptionsController : MonoBehaviour {
         easyDiff.SetActive(false);
         mediumDiff.SetActive(false);
         hardDiff.SetActive(false);
-
-        //easyDiff = GameObject.Find("Easy").GetComponent<Text>();
-        //mediumDiff = GameObject.Find("Medium").GetComponent<Text>();
-        //hardDiff = GameObject.Find("Hard").GetComponent<Text>();
-        if (PlayerPrefsManager.GetDifficulty() == 0f)
+        
+        /*       
+        if (PlayerPrefsManager.GetDifficulty() >= 0f && PlayerPrefsManager.GetDifficulty() < 0.5f)
                 {
-                    easyDiff.SetActive(true);
-                    //mediumDiff.SetActive(false);
-                    //hardDiff.SetActive(false);
+                    easyDiff.SetActive(true);                    
                 }
-                else if (PlayerPrefsManager.GetDifficulty() == 0.5f)
-                {
-                    //easyDiff.SetActive(false);
-                    mediumDiff.SetActive(true);
-                    //hardDiff.SetActive(false);
+                else if (PlayerPrefsManager.GetDifficulty() >= 0.5f && PlayerPrefsManager.GetDifficulty() < 1.0f)
+                {                    
+                    mediumDiff.SetActive(true);                    
                 }
-                else if (PlayerPrefsManager.GetDifficulty() == 1.0f)
-                {
-                    //easyDiff.SetActive(false);
-                    //mediumDiff.SetActive(false);
+                else if (PlayerPrefsManager.GetDifficulty() >= 0.85f)
+                {                   
                     hardDiff.SetActive(true);
                 }
+        */
 
         musicManager = GameObject.FindObjectOfType<MusicPlayer>(); // Find the music manager so I can access it.
         currentVolume = PlayerPrefsManager.GetMasterVolume(); // Get the saved value of the volume.
@@ -72,28 +61,48 @@ public class OptionsController : MonoBehaviour {
         
 	}
 
-    public void setDefaults()
-    {
-        musicManager.audioSource.volume = 1.0f;
-        volumeSlider.value = 1.0f;
-        difficulty = 0.5f;
-        difficultySlider.value = 0.5f;
-        //volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
-    }
-
-	
 	// Update is called once per frame
 	void Update ()
     {
         musicManager.audioSource.volume = volumeSlider.value;
         volumeLevel = volumeSlider.value;
-	}
+
+        if (difficultySlider.value >= 0f && difficultySlider.value < 0.5f)
+        {
+            easyDiff.SetActive(true);
+            mediumDiff.SetActive(false);
+            hardDiff.SetActive(false);
+        }
+        else if (difficultySlider.value >= 0.5f && difficultySlider.value < 0.85f)
+        {
+            easyDiff.SetActive(false);
+            mediumDiff.SetActive(true);
+            hardDiff.SetActive(false);
+        }
+        else if (difficultySlider.value >= 0.85f)
+        {
+            easyDiff.SetActive(false);
+            mediumDiff.SetActive(false);
+            hardDiff.SetActive(true);
+        }
+
+
+    }
 
     public void savePrefs()
     {
         PlayerPrefsManager.SetMasterVolume(volumeLevel);
+        PlayerPrefsManager.SetDifficulty(difficultySlider.value);
     }
 
+    public void setDefaults()
+        {
+            musicManager.audioSource.volume = 1.0f;
+            volumeSlider.value = 1.0f;
+            difficulty = 0.5f;
+            difficultySlider.value = difficulty;
+            //volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+        }
 
 
 }
